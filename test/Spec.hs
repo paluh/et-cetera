3d53361815bccf0738135237fef4b222830e68fa
Tomasz Rybarczyk <paluho@gmail.com>
2015-08-15 20:46:58 +0200

    Add collectd test suite to main, project test suite

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,7 +1,8 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 import qualified System.EtCetera.InterfacesSpec as InterfacesSpec
+import qualified System.EtCetera.CollectdSpec as CollectdSpec
 import           Test.Hspec (hspec)
 
 main :: IO ()
-main = hspec InterfacesSpec.suite
+main = hspec (do InterfacesSpec.suite; CollectdSpec.suite;)
